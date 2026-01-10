b617a9a1381afad90f3213a32f32fdaf81d8f8a7
kadir123456 <bilwininc@gmail.com>
2026-01-10 17:17:13 +0300

    1

--- lib/presentation/screens/profile/profile_screen.dart
+++ lib/presentation/screens/profile/profile_screen.dart
@@ -1,6 +1,6 @@
 // Profile Screen - User Info, Subscription, Logout
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
 import '../../theme/app_theme.dart';
